12bf832fd19b780969e39716d5e23bcd9aec4b97
tosunus <cakmarcorp@cakmaroglu.com>
2020-10-22 06:32:55 +0300

    Create logextract.sh

---
+++ logextract.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+username=$1
+tarih=$2
+dizin=$3
+
+find "$dizin" -type f -name "*${tarih}.tar.bz" | while read -r dosya; do
+    filename=$dosya;
+    filename=${filename%.*};
+    filename=${filename%.*};
+    dosyaadi=$(echo $filename |cut -d/ -f5)
+    dosyaadi="${username}-${dosyaadi}"
+    tar -xvf $dosya tmp/logs/${dosyaadi}.tar.gz;
+    tar -xvf tmp/logs/${dosyaadi}.tar.gz --xform='s|^|'"$dosyaadi"'/|S' -C "${dizin}/yedek";
+    echo ${dizin}/yedek/${dosyaadi};
+done
