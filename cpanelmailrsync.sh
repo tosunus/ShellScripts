cb4e2135382a0de01fa3a221be8af42f8e5b22d0
tosunus <cakmarcorp@cakmaroglu.com>
2020-11-06 13:49:44 +0300

    Update cpanelmailrsync.sh

--- cpanelmailrsync.sh
+++ cpanelmailrsync.sh
@@ -6,10 +6,8 @@ for user in $(echo -n "$CP_ACCOUNTS"); do
     domains=$(grep -i ^dns /var/cpanel/users/"${user}" |cut -d= -f2)
     echo "$domains" | while read -r domain; do
         dirnames="/home/${user}/mail/${domain}"
-        echo "$dirnames" |while read -r dirname; do
-            if [[ ! -d $dirname ]]; then
-                echo "$dirname";
-            fi
-        done
+        if [[ ! -d $dirnames ]]; then
+            echo "$dirnames";
+        fi
     done
 done
