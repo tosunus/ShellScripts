187c7cb3c97302290c2ee1676488c75a52f3b068
tosunus <cakmarcorp@cakmaroglu.com>
2020-11-06 14:03:26 +0300

    Update cpanelmailrsync.sh

--- cpanelmailrsync.sh
+++ cpanelmailrsync.sh
@@ -6,7 +6,7 @@ for user in $(echo -n "$CP_ACCOUNTS"); do
     domains=$(grep -i ^dns /var/cpanel/users/"${user}" |cut -d= -f2)
     echo "$domains" | while read -r domain; do
         dirnames="/home/${user}/mail/${domain}"
-        if [[ ! -d $dirnames ]]; then
+        if [[ -d $dirnames ]]; then
             rsync -avz -e "ssh -p PORT" root@IPADDRESS:"${dirnames}"/* "${dirnames}"/;
             /scripts/remove_dovecot_index_files --user "$user";
         fi
