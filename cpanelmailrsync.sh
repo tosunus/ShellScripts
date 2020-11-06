b7e952eed94b10e5853c94b77cf2367b61943d0e
tosunus <cakmarcorp@cakmaroglu.com>
2020-11-06 13:23:59 +0300

    Create cpanelmailrsync.sh

---
+++ cpanelmailrsync.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+CP_ACCOUNTS=$(ls -1A /var/cpanel/users/)
+
+for user in $(echo -n $CP_ACCOUNTS); do
+    domain=$(grep -i ^dns /var/cpanel/users/${user} |cut -d= -f2)
+    for dom in $(echo -n "$domain"); do
+        dirname="/home/${user}/mail/${domain}"
+        if [[ -d $dirname ]]; then
+            rsync -avz -e "ssh -p PORT" root@IPADDRESS:"${dirname}"/* "${dirname}"/;
+            /scripts/remove_dovecot_index_files --user "$user";
+        fi
+    done
+done
